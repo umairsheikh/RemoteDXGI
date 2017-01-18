743b6772b4cef4bd033d1f4addfd9dfa5ecfc9a1
Umair Sheikh <umairsly@gmail.com>
2017-01-18 21:02:46 +0500

    Revert "Working MVP Screen Share"
    This reverts commit 6e36104ec59b16d43de462b2143b4af2da8bd8af.

--- DXGI/MainWindow.xaml.cs
+++ DXGI/MainWindow.xaml.cs
@@ -210,9 +210,8 @@ namespace DXGI_DesktopDuplication
 
                 //this.BGImage.Source = bitmap;
 
-                this.BGImage.Source = bitmap;
                 //if (dispatcher != null)
-                //Dispatcher.BeginInvoke(MainWindow.RefreshUI,bitmap);
+                  Dispatcher.BeginInvoke(MainWindow.RefreshUI,bitmap);
                 //if (ShowRegionOutlines)
                 //{
                 //    var gfx = gdiScreen1.CreateGraphics();
